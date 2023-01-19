bef05195cb1739a32ac2bd7172af321c0484db63
dan derks <derks.dan@gmail.com>
2023-01-19 11:13:51 -0500

    Revert "site updated"
    This reverts commit 4f126c325263769b8ea1900bee49421b6096fbbd.

--- images/to_convert/convert-jpeg.sh
+++
@@ -1,8 +0,0 @@
-echo ">> converting jpegs"
-
-list=$(ls -r ./*.jpeg)
-for file in $list ; do
-  convert -resize 40% ${file} ${file}.jpeg
-done
-
-#  convert ${file} -define jpeg:extent=512kb ${file}.jpeg
