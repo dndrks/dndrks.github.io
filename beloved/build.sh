f2ad0d9a6efa64734f8ea75e70357c3df649ca38
dan derks <derks.dan@gmail.com>
2023-02-26 13:51:52 -0500

    site updated

--- beloved/build.sh
+++ beloved/build.sh
@@ -26,10 +26,9 @@ for file in $list ; do
   cat head.htm_ > ${target}
   cmark --unsafe ${file}.md >> ${target}
   cat foot.htm_ >> ${target}
-  # sed -i '' -e 's/$pattern/$replacement/g' ${target}
-  sed -i -e 's|BKG_COLOR|'$bkg'|g' ${target}
-  sed -i -e 's|FONT_COLOR|'$font'|g' ${target}
-  sed -i -e 's|HOVER_COLOR|'$hover'|g' ${target}
-  sed -i -e 's|DOT_COLOR|'$dot'|g' ${target}
+  sed -i '' 's|BKG_COLOR|'$bkg'|g' ${target}
+  sed -i '' 's|FONT_COLOR|'$font'|g' ${target}
+  sed -i '' 's|HOVER_COLOR|'$hover'|g' ${target}
+  sed -i '' 's|DOT_COLOR|'$dot'|g' ${target}
   sed -i '' -e 's#DATE#'$date'#g' ${target}
 done
